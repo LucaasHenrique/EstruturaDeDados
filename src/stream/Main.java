5f95db2aafe786b66d0d1367ab5494d8926ec9ff
LucaasHenrique <lucashsouza101@gmail.com>
2024-09-09 11:35:04 -0300

    streams

---
+++ src/stream/Main.java
@@ -0,0 +1,14 @@
+package stream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> frutas = new ArrayList<>();
+        frutas.add("maça");
+        frutas.add("banana");
+
+        frutas.stream().forEach(String::new);
+    }
+}
