5cec8e0605f0bdaa172b3e3dff498936e3a1baaf
LucaasHenrique <lucashsouza101@gmail.com>
2024-11-15 14:25:40 -0300

    binary search

--- src/EstruturaDeDados/ordenacao/pesquisabinaria/BinarySearch.java
+++ src/EstruturaDeDados/ordenacao/pesquisabinaria/BinarySearch.java
@@ -1,29 +1,29 @@
 package EstruturaDeDados.ordenacao.pesquisabinaria;
 
 public class BinarySearch {
- public static void main(String[] args) {
-    int[] list = {1, 3, 5, 7, 9, 13, 17, 20, 26, 33};
-    
-    System.out.println("Número encontrado na " + binarySearch(list, 13)+"° posição!");
-  }
+     public static void main(String[] args) {
+        int[] list = {1, 3, 5, 7, 9, 13, 17, 20, 26, 33};
 
-  public static int binarySearch(int[] list, int item) {
-    int baixo = 0;
-    int alto = list.length - 1;
+        System.out.println("Número encontrado na " + binarySearch(list, 13)+"° posição!");
+     }
 
-    while (baixo <= alto) {
-      int meio = (baixo + alto) / 2;
-      int chute = list[meio];
+     public static int binarySearch(int[] list, int item) {
+            int baixo = 0;
+            int alto = list.length - 1;
 
-      if (chute == item) {
-        return meio;
-      } 
-      if (chute > item) {
-        alto = meio - 1;
-      } else {
-        baixo = meio + 1;
-      }
-    }
-    return 0;
-  }
+            while (baixo <= alto) {
+                  int meio = (baixo + alto) / 2;
+                  int chute = list[meio];
+
+                  if (chute == item) {
+                    return meio;
+                  }
+                  if (chute > item) {
+                    alto = meio - 1;
+                  } else {
+                    baixo = meio + 1;
+                  }
+            }
+            return 0;
+     }
 }
