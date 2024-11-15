95544c4b587e330f4c8664ee3b642f8bb4c2cba5
LucaasHenrique <lucashsouza101@gmail.com>
2024-11-15 14:23:39 -0300

    exercicio4

--- src/EstruturaDeDados/ordenacao/exercicios/exercicio4/Aluno.java
+++ src/EstruturaDeDados/ordenacao/exercicios/exercicio4/Aluno.java
@@ -1,4 +1,14 @@
 package EstruturaDeDados.ordenacao.exercicios.exercicio4;
 
 public class Aluno {
+
+    String name;
+    int nota1;
+    int nota2;
+
+    public Aluno(String name, int nota1, int nota2) {
+        this.name = name;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
 }
