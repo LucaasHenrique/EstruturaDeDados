7a69daa29e6b73686337ca967a5d30c4cc3cfc6e
LucaasHenrique <lucashsouza101@gmail.com>
2025-02-13 15:19:57 -0300

    exercico3

--- src/EstruturaDeDados/estruturas/listas/exercicios/exercicio3/Exercicio3.java
+++ src/EstruturaDeDados/estruturas/listas/exercicios/exercicio3/Exercicio3.java
@@ -1,4 +1,24 @@
 package EstruturaDeDados.estruturas.listas.exercicios.exercicio3;
 
+import EstruturaDeDados.estruturas.listas.exercicios.exercicio2.ListaFuncionario;
+
+import java.util.Scanner;
+
 public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ListaAlunos alunos = new ListaAlunos();
+
+        System.out.println("Cadastro de Aluno");
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("Aluno %d%n", i + 1);
+            System.out.print("Nome: ");
+            String nome = scanner.next();
+            System.out.print("Nota: ");
+            double nota = scanner.nextDouble();
+            alunos.inserirAluno(nome, nota);
+        }
+
+        alunos.aprovados();
+    }
 }
--- src/EstruturaDeDados/estruturas/listas/exercicios/exercicio3/ListaAlunos.java
+++ src/EstruturaDeDados/estruturas/listas/exercicios/exercicio3/ListaAlunos.java
@@ -1,4 +1,59 @@
 package EstruturaDeDados.estruturas.listas.exercicios.exercicio3;
 
+//Lista duplamente encadeada e não ordenada
 public class ListaAlunos {
+
+    private static class Alunos {
+
+        String nome;
+        double notaFinal;
+        Alunos proximo;
+        Alunos anterior;
+
+        public Alunos(String nome, double notaFinal) {
+            this.nome = nome;
+            this.notaFinal = notaFinal;
+        }
+    }
+
+    Alunos inicio = null;
+    Alunos fim = null;
+    Alunos atual;
+
+    public void inserirAluno(String nome, double notaFinal) {
+        Alunos novo = new Alunos(nome, notaFinal);
+
+        if (inicio == null) {
+            inicio = fim = novo;
+            novo.proximo = null;
+            novo.anterior = null;
+        } else {
+            fim.proximo = novo;
+            novo.anterior = fim;
+            novo.proximo = null;
+            fim = novo;
+        }
+    }
+
+    public void aprovados() {
+        atual = inicio;
+        int cont = 0;
+        if (inicio == null) {
+            System.out.println("Lista vazia!!");
+        } else {
+            System.out.println("Consultando a lista do inicio ao fim");
+            atual = inicio;
+            while (atual != null) {
+                if (atual.notaFinal > 7) {
+                    System.out.println("Nome: " + atual.nome + " Nota: " + atual.notaFinal + " Situação: aprovado");
+                    cont += 1;
+                }
+                atual = atual.proximo;
+            }
+
+            if (cont == 0) {
+                System.out.println("Nenhuma aluno aprovado!!");
+            }
+        }
+    }
 }
