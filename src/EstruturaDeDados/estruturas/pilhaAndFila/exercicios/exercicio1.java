caa88aef8802fc1680be66362ee583a5d96dbe18
LucaasHenrique <lucashsouza101@gmail.com>
2025-02-22 17:22:06 -0300

    exercicios

--- src/EstruturaDeDados/estruturas/pilhaAndFila/exercicios/exercicio1.java
+++ src/EstruturaDeDados/estruturas/pilhaAndFila/exercicios/exercicio1.java
@@ -1,4 +1,29 @@
 package EstruturaDeDados.estruturas.pilhaAndFila.exercicios;
 
+import EstruturaDeDados.estruturas.pilhaAndFila.fila.Fila;
+import EstruturaDeDados.estruturas.pilhaAndFila.pilha.Pilha;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class exercicio1 {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<>();
+        Pilha<Integer> pilha = new Pilha<>();
+
+        pilha.inserir(4);
+        pilha.inserir(6);
+        pilha.inserir(10);
+        pilha.inserir(3);
+        pilha.inserir(12);
+
+        fila.inserir(5);
+        fila.inserir(15);
+        fila.inserir(132);
+        fila.inserir(21);
+        fila.inserir(243);
+
+        pilha.consultarPilha();
+        fila.consultarFila();
+    }
 }
