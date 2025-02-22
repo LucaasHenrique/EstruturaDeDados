f70419b5a3143bdd061d9af2f9d158ea766c230a
LucaasHenrique <lucashsouza101@gmail.com>
2025-02-22 17:23:01 -0300

    two points beecrowd

--- src/EstruturaDeDados/problems/TwoPoints.java
+++ src/EstruturaDeDados/problems/TwoPoints.java
@@ -1,4 +1,19 @@
 package EstruturaDeDados.problems;
 
+import java.util.Scanner;
+
 public class TwoPoints {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        double x1, y1, x2, y2;
+
+        x1 = in.nextDouble();
+        y1 = in.nextDouble();
+        x2 = in.nextDouble();
+        y2 = in.nextDouble();
+
+        double distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.printf("%.2f", distancia);
+    }
 }
